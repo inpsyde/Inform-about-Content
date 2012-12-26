fcc03a2e91dcf0e955eaaca13f90d2ee37403e24
David Naber <kontakt@dnaber.de>
2012-12-26 15:20:53 +0100

    increase version to current dev status

--- inform-about-content.php
+++ inform-about-content.php
@@ -6,7 +6,7 @@
  * Domain Path: /languages
  * Description: Informs all users of a blog about a new post and approved comments via email
  * Author:      Inpsyde GmbH
- * Version:     0.0.5-master
+ * Version:     0.0.6-master
  * License:     GPLv3
  * Author URI:  http://inpsyde.com/
  */
